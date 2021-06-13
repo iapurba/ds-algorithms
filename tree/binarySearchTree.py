cfd4bc76cc0f3ddf8f5323c93916899fbc8b2152
Apurba Panja <apurbapanja1@gmail.com>
2021-06-13 18:05:46 +0530

    Implemented breadth first search functionality for binary tree

--- tree/binarySearchTree.py
+++ tree/binarySearchTree.py
@@ -62,64 +62,103 @@ def delete_node(root, key):
     return root
 
 
+# Depth first traversal preorder, inroder & postorder.
+# Here we have taken an extra array as parameter to store the node value,
+# so the it will help to visualize the order
+
 # Preorder traversal
-def preorder(root):
+def dfs_preorder(root, result):
     if root:
         # Visit node
-        print(str(root.key), "-->", end=" ")
+        # And push the node value into the result list
+        result.append(root.key)
 
         # Visit left
-        preorder(root.left)
+        dfs_preorder(root.left, result)
 
         # Visit right
-        preorder(root.right)
+        dfs_preorder(root.right, result)
+
+    return result
 
 
 # Inorder traversal
-def inorder(root):
+def dfs_inorder(root, result):
     if root:
         # Visit left
-        inorder(root.left)
+        dfs_inorder(root.left, result)
 
         # Visit node
-        print(str(root.key), '-->', end=' ')
+        # And push the node value into the result list
+        result.append(root.key)
 
         # Visit right
-        inorder(root.right)
+        dfs_inorder(root.right, result)
+    
+    return result
 
 
 # Postorder traversal
-def postorder(root):
+def dfs_postorder(root, result):
     if root:
         # Visit left
-        postorder(root.left)
+        dfs_postorder(root.left, result)
 
         # Visit right
-        postorder(root.right)
+        dfs_postorder(root.right, result)
 
         # Visit node
-        print(str(root.key), "-->", end=" ")
+        # And push the node value in the result list
+        result.append(root.key)
+    
+    return result
 
 
+# Breadth first traversal
+def bfs(root):
+    if not root:
+        return
+    
+    queue = []
+    queue.append(root)
+    result = []
+
+    while queue:
+        current = queue.pop(0)
+        result.append(current.key)
+        
+        if current.left:
+            queue.append(current.left)
+
+        if current.right:
+            queue.append(current.right)
+    
+    return result
+
+    
+    
 if __name__ == "__main__":
     root = None
 
-    root = insert(root, 15)
     root = insert(root, 10)
+    root = insert(root, 7)
     root = insert(root, 11)
-    root = insert(root, 13)
-    root = insert(root, 26)
-    root = insert(root, 17)
-    root = insert(root, 31)
-    root = insert(root, 2)
     root = insert(root, 6)
-    root = insert(root, 4)
+    root = insert(root, 1)
+    root = insert(root, 8)
+    root = insert(root, 9)
+    root = insert(root, 20)
+    root = insert(root, 22)
+    root = insert(root, 14)
 
-    # delete_node(root, 17)
+    # delete_node(root, 9)
 
-    preorder(root)
-    print()
-    inorder(root)
-    print()
-    postorder(root)
-    print()
+    # Remeber as above the functions defination,
+    # we must send and empty array to store the visited node values
+    print("DFS Preorder:", dfs_preorder(root, []))
+
+    print("DFS Inorder:", dfs_inorder(root, []))
+
+    print("DFS Postorder:", dfs_inorder(root, []))
+
+    print("Breadth first traversal:", bfs(root))
